a95b932da34cd57e011e0b0a783092c38fd59b82
Higashi Yonamine <e165714@ie.u-ryukyu.ac.jp>
2016-12-01 13:25:56 +0900

    programming2,report5 at step1.

---
+++ src/jp/ac/uryuyku/ie/e165714/report5.java
@@ -0,0 +1,11 @@
+package jp.ac.uryuyku.ie.e165714;
+
+/**
+ * Created by yonaminehigashi on 2016/12/01.
+ */
+public class report5 {
+    public static void main(String[] args){
+        String str =  null;
+        System.out.println(str.length());
+    }
+}
