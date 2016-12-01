bf483fb1fae475c92987397e8c14bbd2cdce6d02
Higashi Yonamine <e165714@ie.u-ryukyu.ac.jp>
2016-12-01 15:03:07 +0900

    programming2,report5 at step2.

--- src/jp/ac/uryuyku/ie/e165714/report5.java
+++ src/jp/ac/uryuyku/ie/e165714/report5.java
@@ -4,8 +4,15 @@ package jp.ac.uryuyku.ie.e165714;
  * Created by yonaminehigashi on 2016/12/01.
  */
 public class report5 {
+
     public static void main(String[] args){
-        String str =  null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }catch (java.lang.NullPointerException e){
+            System.out.println("java.lang.NullPointerException is emerge");
+            e.printStackTrace();
+        }
+
     }
 }
